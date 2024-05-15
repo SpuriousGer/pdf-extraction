3338943bbff70d761e8b048d6044d92bd70f0d0d
SpuriousGer <24923457+SpuriousGer@users.noreply.github.com>
2024-05-15 09:26:45 +0200

    initial commit

--- app.py
+++ app.py
@@ -10,11 +10,15 @@ def print_form_fields(pdf_path):
         # Get the form fields on the current page
         for widget in page.widgets():
             # Print field name and value
-            print(f"Field Name: {widget.field_name}, Field Value: {widget.field_value}")
-
+            # print(f"Field Name: {widget.field_name}, Field Value: {widget.field_value}")
+            print(widget.field_name)
     # Close the document
     doc.close()
 
+def fill_pdf_from_form(pdf_name, form_data):
+    pdf_path = f'pdfs/{pdf_name}'
+    doc = fitz.open(pdf_path)
+
 
 # Replace 'your_file.pdf' with the path to your PDF
-print_form_fields('pdfs/D0010.pdf')
+print_form_fields('pdfs/D0003AL.pdf')
